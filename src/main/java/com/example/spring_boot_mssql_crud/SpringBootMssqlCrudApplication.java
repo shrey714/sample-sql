cd00e541bea4f87ed6564fea5c7bc6625581b1c2
shrey <pshrey0000@gmail.com>
2025-10-30 22:44:34 +0530

    first commit

---
+++ src/main/java/com/example/spring_boot_mssql_crud/SpringBootMssqlCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_mssql_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMssqlCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMssqlCrudApplication.class, args);
+	}
+
+}
