844e265eb83c10b5ea6729d1c21826be3ffeb9fc
Shrey Patel <76216765+shrey714@users.noreply.github.com>
2025-11-11 14:53:23 +0530

    Update SpringBootMssqlCrudApplication.java

--- src/main/java/com/example/spring_boot_mssql_crud/SpringBootMssqlCrudApplication.java
+++ src/main/java/com/example/spring_boot_mssql_crud/SpringBootMssqlCrudApplication.java
@@ -11,3 +11,6 @@ public class SpringBootMssqlCrudApplication {
 	}
 
 }
+
+
+Hi [Manager’s Name], I’m planning to take leave from 24th Nov to 5th Dec. I checked with Adi — he’ll be done with the copilot project before then. Just wanted to confirm if that’s okay with you.
